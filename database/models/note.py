cc5f1b7c7dce8634204eed2737e1c48c4f7f1c9f
Davi Coelho <davi.coelho_comp@outlook.com>
2024-08-12 16:49:14 -0300

    Update note.py

--- database/models/note.py
+++ database/models/note.py
@@ -1,4 +1,15 @@
+from __future__ import annotations
+from database.models.base import BASE
+from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 
+class Note(BASE):
+    __tablename__ = "notes"
 
-class Note():
-    pass
+    id_note: Mapped[int] = mapped_column(primary_key=True)
+    str_text: Mapped[str] = mapped_column(String)
+    str_md_text: Mapped[str] = mapped_column(String)
+    str_date: Mapped[str] = mapped_column(String)
+    bool_completed: Mapped[bool] = mapped_column(Boolean)
+    
